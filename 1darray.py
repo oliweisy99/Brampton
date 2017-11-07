38d77e9bf5ab0359bc7642ce4bbb3d44f834e4cd
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-11-07 13:55:12 +0000

    array stuff

--- 1darray.py
+++ 1darray.py
@@ -1,1 +1,12 @@
+# DECLARE ages : ARRAY[0..9] OF INTEGER
+ages = [56,54,64,23,56,23,64,32,12,51]
+index = 0
+count = 0
 
+for count in range(0,8) :
+    for index in range(0,9):
+        if ages[index] > ages[index + 1]:
+            temp = ages[index]
+            ages[index] = ages[index + 1]
+            ages[index + 1] = temp
+print(ages)
--- hi.py
+++ hi.py
@@ -1,2 +1,12 @@
-Print("Hello there")
-# hello there comment
+# details details
+# declare firstName : STRING
+# declare surname : STRING
+# declare age : INTEGER
+# declare PT : STRING
+firstName = input("Enter your first Name: ")
+surname = input("Enter your surname")
+age = int(input("Enter your age"))
+PT = input("Enter your PT")
+print(firstName, surname)
+print("Age: ", age)
+print("PT is: ", PT)
