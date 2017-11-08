e7da91ff104c8495c34a1e4ded353843340eb713
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-11-08 09:31:09 +0000

    array

--- 1darray.py
+++ 1darray.py
@@ -1,20 +1,33 @@
 # DECLARE ages : ARRAY[0..9] OF INTEGER
 # ages = [56,54,64,23,56,23,64,32,12,51]
-ages = []
+
 index = 0
 count = 0
 
-for index in range(0,10) :
+def get_valid_age(): #RETURNS temp
     temp = int(input("Enter age (0-99) "))
     while temp < 0 or temp > 99:
         print("Error.")
         temp = int(input("Enter age (0-99) "))
-    ages.append(temp)
+    return temp
 
-for count in range(0,8) :
-    for index in range(0,9):
-        if ages[index] > ages[index + 1]:
-            temp = ages[index]
-            ages[index] = ages[index + 1]
-            ages[index + 1] = temp
-print(ages)
+def bubble_sort():
+    for count in range(0, 9):
+        for index in range(0,9):
+            if ages[index] > ages[index + 1]:
+                temp = ages[index]
+                ages[index] = ages[index + 1]
+                ages[index + 1] = temp
+
+def output_array_contents():
+    for count in range(0,10):
+        print(ages[count])
+
+ages = []
+for index in range(0,10) :
+    ages.append(get_valid_age())
+bubble_sort()
+output_array_contents()
+
+
+
