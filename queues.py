37da14c0c4df5a12230a776cb274ae50db528092
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-11-22 10:04:39 +0000

    First version

---
+++ queues.py
@@ -0,0 +1,43 @@
+startPos = 0
+endPos = -1
+thequeue = []
+
+def enqueue(thequeue, item): # puts an item into the queue
+    global endPos
+    endPos = endPos + 1
+    thequeue.append(item)
+    # thequeue[endPos] = item
+
+def dequeue(thequeue): # takes the first item of the queue
+    # DECLARE output : CHAR
+    global startPos, endPos
+    if startPos == endPos + 1:
+        output = 'END OF QUEUE'
+    else:
+        output = thequeue[startPos]
+        startPos = startPos + 1
+    return output
+
+def processqueue(character, thequeue): # display the first item in the queue in lower case
+    character = dequeue(thequeue) #NOT SURE WHAT TO DO HERE - i had dequeue but didnt make sense
+    print(character.lower())
+
+def queueempty(): # tests to see if the queue is empty
+    global startPos, endPos, Flag
+    if startPos == endPos + 1 :
+        Flag = True
+    else:
+        Flag = False
+    return Flag
+
+
+# new main program
+count = 0
+while True: # doesnt work????
+    character = input("enter character")
+    enqueue(thequeue, character)
+    count = count + 1
+    if count >= 5:
+        processqueue(character, thequeue)
+    if queueempty() == True :
+        break
