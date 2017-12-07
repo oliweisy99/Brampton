98d63e5047d08e5db484fb5c78fa1a8543891cd1
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-07 09:51:20 +0000

    First version

--- JanAssess.py
+++ JanAssess.py
@@ -13,8 +13,24 @@ for i in range(0,4) :
 first = random.choice(nameArr)
 second = random.choice(nameArr)
 ranNum = random.randint(0,10)
+ranNumTwo = random.randint(0,10)
 
+def askRep(numRan, team) :
+    global count
+    count = 0
+    while True :
+        print(numRan)
+        guess = int(input(team + ": guess a number between 1 and 10"))
+        if guess == numRan :
+            break
+        else :
+            count = count + 1
+    return count
 
-# while True :
-  #  count = 0
-guess = int(input(first + ": guess a number between 1 and 10"))
+teamOne = askRep(ranNum, first)
+print(first, " : ", teamOne)
+teamTwo = askRep(ranNumTwo, second)
+print(second, " : ", teamTwo)
+
+if teamTwo > teamOne :
+    print("")
