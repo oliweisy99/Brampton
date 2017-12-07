c46b0316df326b3af900b41b2fea7b8451587ee6
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-07 09:37:17 +0000

    First version

--- JanAssess.py
+++ JanAssess.py
@@ -2,6 +2,19 @@
 # Oliver Weisfeld
 # 7-12-17
 
-nameArr = []
+import random
+
+nameArr = ["","","",""]
 
 for i in range(0,4) :
+    nameArr[i] = input("enter the name of the team: ")
+
+
+first = random.choice(nameArr)
+second = random.choice(nameArr)
+ranNum = random.randint(0,10)
+
+
+# while True :
+  #  count = 0
+guess = int(input(first + ": guess a number between 1 and 10"))
