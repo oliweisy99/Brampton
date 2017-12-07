051a50f184a3417f9383dc5695673badae210ba5
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-07 15:05:30 +0000

    Finished

--- JanAssess.py
+++ JanAssess.py
@@ -9,18 +9,6 @@ nameArr = ["","","",""]
 for i in range(0,4) :
     nameArr[i] = input("enter the name of the team: ") # put names into array
 
-first = random.choice(nameArr) # picking random names from array
-second = random.choice(nameArr)
-ranNum = random.randint(0,10) # picking two random numbers
-ranNumTwo = random.randint(0,10)
-
-# def choice(f, s, rn, rnt) :
-# f = ...
-# s = ...
-# rn = ...
-# rnt = ...
-
-
 def askRep(numRan, team) : # getting count for guesses
     global count
     count = 0
@@ -33,43 +21,34 @@ def askRep(numRan, team) : # getting count for guesses
     return count
 
 
-teamOne = askRep(ranNum, first)
-print(first, " : ", teamOne)
-teamTwo = askRep(ranNumTwo, second)
-print(second, " : ", teamTwo) # printing counts for guesses
-
-# def printCount(ar(), f, s, rnt, rn) :
-    # global teamone, teamTWo
-    # teamOne = ar(rno, f)
-    # print ...
-    # teamTwo = ar(rnt, s)
-    #print...
+def printCount(ar, f, s, rno, rnt) :
+    global teamOne, teamTwo
+    teamOne = ar(rno, f)
+    print(f, " : ", teamOne)
+    teamTwo = ar(rnt, s)
+    print(s, " : ", teamTwo)
 
 
+def decision(to, tt, f, s) :
+    if tt > to :
+        print("team ", f, "wins")
+    elif tt < to :
+        print("team ", s, " wins")
+    else :
+        print("draw")
+
+while True :
+    first = random.choice(nameArr)
+    second = random.choice(nameArr)
+    ranNum = random.randint(1,10)
+    ranNumTwo = random.randint(1,10)
+    printCount(askRep, first, second, ranNum, ranNumTwo)
+    decision(teamOne, teamTwo, first, second)
+    quit = int(input("Do you want to quit? [1 = yes, 0 = no]"))
+    if quit == 1 :
+        break
+    else :
+        continue
 
 
-if teamTwo > teamOne : # deciding who wins
-    print("team ", first, " wins")
-elif teamTwo < teamOne :
-    print("team ", second, " wins")
-else :
-    print("draw")
 
-# def decision(to, tw, f, s) :
-    # if tt > to :
-        # pr(..f.)
-        #...
-
-
-
-# while True :
-    # choice(first, second, ranNum, ranNumTwo)
-    # printCount(askRep(), first, second, ranNumTwo, ranNum)
-    # decision(teamOne, teamTwo, first, second)
-    # quit = int(input(("would you like to quit[ 1 = yes, 0, no]"))
-        # if quit = 1
-            # break
-        # else :
-            # continue
-
-# this should wrk
