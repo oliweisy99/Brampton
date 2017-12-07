15e1942b143959686d86582e7c963fc3f6b0c229
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-07 10:03:49 +0000

    First version

--- JanAssess.py
+++ JanAssess.py
@@ -7,20 +7,18 @@ import random
 nameArr = ["","","",""]
 
 for i in range(0,4) :
-    nameArr[i] = input("enter the name of the team: ")
+    nameArr[i] = input("enter the name of the team: ") # put names into array
 
-
-first = random.choice(nameArr)
+first = random.choice(nameArr) # picking random names from array
 second = random.choice(nameArr)
-ranNum = random.randint(0,10)
+ranNum = random.randint(0,10) # picking two random numbers
 ranNumTwo = random.randint(0,10)
 
-def askRep(numRan, team) :
+def askRep(numRan, team) : # getting count for guesses
     global count
     count = 0
     while True :
-        print(numRan)
-        guess = int(input(team + ": guess a number between 1 and 10"))
+        guess = int(input(team + ": guess a number between 1 and 10: "))
         if guess == numRan :
             break
         else :
@@ -30,7 +28,14 @@ def askRep(numRan, team) :
 teamOne = askRep(ranNum, first)
 print(first, " : ", teamOne)
 teamTwo = askRep(ranNumTwo, second)
-print(second, " : ", teamTwo)
+print(second, " : ", teamTwo) # printing counts for guesses
 
-if teamTwo > teamOne :
-    print("")
+
+if teamTwo > teamOne : # deciding who wins
+    print("team ", first, " wins")
+elif teamTwo < teamOne :
+    print("team ", second, " wins")
+else :
+    print("draw")
+
+
