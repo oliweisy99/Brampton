4c6fc302e3b5762db7fe3f9ef681b8bd469b6e75
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-20 18:38:52 -0400

    Finished version

--- queues.py
+++ queues.py
@@ -3,24 +3,25 @@ endPos = -1
 thequeue = []
 
 def enqueue(thequeue, item): # puts an item into the queue
-    global endPos
-    endPos = endPos + 1
-    thequeue.append(item)
+    global endPos # global so that other procedures/functions can use the same end position
+    endPos = endPos + 1 # end position is increased by one as one new item has been added to the queue
+    thequeue.append(item)  # adds inputted item into the queue
     # thequeue[endPos] = item
 
 def dequeue(thequeue): # takes the first item of the queue
     # DECLARE output : CHAR
     global startPos, endPos
     if startPos == endPos + 1:
-        output = 'END OF QUEUE'
+        output = "END OF QUEUE" # if the end of queue has been reached as pointers have been moved, then its the end
     else:
-        output = thequeue[startPos]
-        startPos = startPos + 1
+        output = thequeue[startPos] # returns the first position of the queue - output
+        startPos = startPos + 1 # start position pointer is moved up so the next item will be outputted next time round
     return output
 
 def processqueue(character, thequeue): # display the first item in the queue in lower case
     character = dequeue(thequeue)
-    print(character.lower())
+    print(character.lower()) # outputs first position of queue from the "De-queue" routine
+# setting it to lower case is not necessary, so you can print the first position however you like.
 
 def queueempty(): # tests to see if the queue is empty
     global startPos, endPos, Flag
