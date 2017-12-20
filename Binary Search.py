0c069b14f5c7b68008b267970b0a121e3acc79e6
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-20 18:16:12 -0400

    Finished version

--- Binary Search.py
+++ Binary Search.py
@@ -1,18 +1,24 @@
+'''
+repeated checking of the middle item in an ordered search list and discarding the half of the list
+which does not contain the search item
+'''
+
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15]
+# set array of values, values can be changed but must be ordered
 
 target = 8
 found = False
 startPos = 0
 endPos = 9
 
-while found == False and startPos <= endPos:
+while found == False and startPos <= endPos: # start position can't be larger than end position
     midPos = (startPos + endPos) // 2  # double // returns integer value... as single / returns float
     if target == number[midPos]:
         found = True
     elif target < number[midPos]:
-        endPos = midPos - 1
+        endPos = midPos - 1 # if target is lower down than mid, then end position changes
     else:
-        startPos = midPos + 1
+        startPos = midPos + 1 # if target is higher up than mid, then start position changes
 
 print(found)
 
