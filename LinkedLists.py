2bdc39648263458ccd9699fe2cfc98bde4cf5ef5
OliverWeisfeld <oliverweisfeld@gmail.com>
2018-01-03 20:49:18 +0000

    Start of linked List work

--- LinkedLists.py
+++ LinkedLists.py
@@ -9,13 +9,13 @@ class linked_list:
     def __init__(self):
         self.cur_node = None # cur meaning current is set to none
 
-    def add_node(self, data):
+    def add_node(self, data): # add nodes to linked list
         new_node = node() # create a new node
         new_node.data = data
         new_node.pointer = self.cur_node # link the new node to the 'previous' node.
         self.cur_node = new_node #  set the current node to the new one.
 
-    def list_print(self):
+    def list_print(self): # prints out nodes
         node = self.cur_node # cant point to ll!
         while node:
             print(node.data)
