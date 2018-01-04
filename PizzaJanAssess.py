456a6ff5fd58ef8584d7bb60aa3b8b19e6819c6b
OliverWeisfeld <oliverweisfeld@gmail.com>
2018-01-04 09:50:39 +0000

    level 3

--- PizzaJanAssess.py
+++ PizzaJanAssess.py
@@ -19,3 +19,70 @@ for i in range(10) :
     StockL = toppings[i][1]
     print(p, " ", topping, " ", StockL)
     p = p + 1
+
+
+while True:
+    base = input("Would you like a (T)hin base or a (P)an base?")
+    if base == "P" :
+        break
+    if base == "p":
+        break
+    if base == "T" :
+        break
+    if base == "t" :
+        break
+    else :
+        print("invalid input, try again")
+        continue
+
+topChoices = [0, 1, 2, 3, 4]
+while True:
+    tChoice = int(input("select a topping choice from the list [0-9]"))
+    if tChoice < 0 :
+        print("not valid topping, try again")
+        continue
+    if tChoice > 9 :
+        print("not valid topping, try again")
+        continue
+    else :
+        toppings[tChoice - 1][1] -= toppings[tChoice - 1][1]
+        if toppings[tChoice - 1][1] >= 1:
+            continue
+        else :
+            print(toppings[tChoice - 1][0], " ", "is out of stock")
+        topChoices[0] = tChoice
+        break
+
+
+
+
+'''
+class node:
+    def __init__(self):
+        self.No = None # contains the data
+        self.Bs = None # contains the reference to the next node
+        self.t = [0, 1, 2, 3, 4]
+
+class linked_list:
+    def __init__(self):
+        self.cur_node = None # cur meaning current
+
+    def add_node(self, data): # add nodes to linked list
+        new_node = node() # create a new node
+        new_node.data = data
+        new_node.pointer = self.cur_node # link the new node to the 'previous' node.
+        self.cur_node = new_node # set the current node to the new one.
+
+    def list_print(self): # prints out nodes
+        node = self.cur_node  # cant point to ll!
+        while node:
+            print(node.data)
+            node = node.pointer
+
+
+ll = linked_list()
+ll.add_node(1)
+ll.add_node(2)
+ll.add_node(3)
+ll.list_print()
+'''
