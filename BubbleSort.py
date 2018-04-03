578a5671bef1d0aacb8f91ce5e13651a1f5a7e41
OliverWeisfeld <oliverweisfeld@gmail.com>
2018-04-03 19:16:05 +0100

    Working version

--- BubbleSort.py
+++ BubbleSort.py
@@ -11,6 +11,6 @@ def shortBubbleSort(alist):
                alist[i+1] = temp
        passnum = passnum-1
 
-alist=[20,30,40,90,50,60,70,80,100,110, 44]
+alist=[20,30,40,90,50,60,70,80,100,110,44]
 shortBubbleSort(alist)
 print(alist)
--- InsertionSort.py
+++ InsertionSort.py
@@ -1,22 +1,22 @@
-'''
-from input, this takes your numbers, puts them into an array then re-orders them into ascending value
-'''
+
+# from input, this takes your numbers, puts them into an array then re-orders them into ascending value
 
 index = 2
 count = 1
 myArray = [0, 1, 2, 3, 4, 5]
 
-for count in range(0,5):
+for count in range(0,len(myArray) - 1):
     item = int(input("what is your input?"))
     myArray[count + 1] = item
 # takes input and stores input into array
 
-for index in range(index, len(myArray)):
-    insertItem = myArray[index]
+for index in range(len(myArray)): # to go through each value in the array
+    insertItem = myArray[index] # stores value which gets replaced
     currentitem = index - 1
-    while myArray[currentitem] > insertItem and currentitem > 0:
+    while myArray[currentitem] > insertItem and currentitem > 0: # going through individual value compared to other values
         myArray[currentitem + 1] = myArray[currentitem]
         currentitem = currentitem - 1
+        print(myArray)
     myArray[currentitem + 1] = insertItem
 # goes through each number in the array and loops until the number is smaller than the one in front
 # switches the number round if the count number is bigger than the number after it in the array
