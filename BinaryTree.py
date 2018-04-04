bcf3022a3ab400fa499d49f9e2729f46cce3bb64
OliverWeisfeld <oliverweisfeld@gmail.com>
2018-04-04 13:29:32 +0100

    Working version

--- BinaryTree.py
+++ BinaryTree.py
@@ -19,4 +19,62 @@ def initialiseTree():
     tree[6].leftPointer = nullPointer
 
 initialiseTree()
-print(tree[6].leftPointer)
+
+def insertNode(newItem):
+    global freePtr, nullPointer, newNodePtr, rootPointer, thisNodePtr, previousNodePtr, turnedLeft
+    if freePtr != nullPointer:
+        newNodePtr = freePtr
+        freePtr = tree[freePtr].leftPointer
+        tree[newNodePtr].data = newItem
+        tree[newNodePtr].leftPointer = nullPointer
+        tree[newNodePtr].rightPointer = nullPointer
+
+        if rootPointer == nullPointer:
+            rootPointer = newNodePtr
+        else:
+            thisNodePtr = rootPointer
+            while thisNodePtr != nullPointer:
+                previousNodePtr = thisNodePtr
+                if tree[thisNodePtr].data > newItem:
+                    turnedLeft = True
+                    thisNodePtr = tree[thisNodePtr].leftPointer
+                else:
+                    turnedLeft = False
+                    thisNodePtr = tree[thisNodePtr].rightPointer
+            if turnedLeft == True:
+                tree[previousNodePtr].leftPointer = newNodePtr
+            else:
+                tree[previousNodePtr].rightPointer = newNodePtr
+
+
+insertNode("a")
+insertNode("z")
+insertNode("d")
+insertNode("c")
+insertNode("b")
+insertNode("f")
+print("data:", tree[0].data)
+print("leftp:", tree[0].leftPointer)
+print("rightp:", tree[0].rightPointer)
+print("data:", tree[1].data)
+print("leftp:", tree[1].leftPointer)
+print("rightp:", tree[1].rightPointer)
+print("data:", tree[2].data)
+print("leftp:", tree[2].leftPointer)
+print("rightp:", tree[2].rightPointer)
+print("data:", tree[3].data)
+print("leftp:", tree[3].leftPointer)
+print("rightp:", tree[3].rightPointer)
+print("data:", tree[4].data)
+print("leftp:", tree[4].leftPointer)
+print("rightp:", tree[4].rightPointer)
+print("data:", tree[5].data)
+print("leftp:",tree[5].leftPointer)
+print("rightp:",tree[5].rightPointer)
+print("data:", tree[6].data)
+print("leftp:",tree[6].leftPointer)
+print("rightp:",tree[6].rightPointer)
+
+
+
+
